b86032cd3adae5b868e6b1e9cfee59fa757fe059
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-27 15:42:20 +0600

    32-7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { buildQueries } from '@testing-library/dom';
 
 function App() {
+  var person = {
+    name :"Istiaqe Ahmed Khan",
+    job: "Web Developer"
+  }
+  var style={
+    color: 'red',
+    backgroundColor: 'blue'
+  }
+  const heroes= ['Anwar', 'Badshah', 'Elias','Salman']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{color:'red'}}>Famous Hero and His Heroin</p>
+      <Person name= {heroes[0]}Heroin="MunMun"></Person>
+      <Person name="Alamgir" Heroin="Shabana"></Person>
+      <Person name="kabila" Heroin="Nachrin"></Person>
+      <Person name="Jasim" Heroin="Bobita"></Person>
+          
       </header>
     </div>
   );
+}
+function Person(props){
+  const personStyle={
+    border:'5px solid green',
+    borderRadius:'10px',
+    boxShadow: '0px 0px 30px 30px blue ',
+    width: '250px',
+    margin: '10px'
+  }
+  // console.log(props)
+  return (
+  <div style={personStyle}>
+    <h1  style={{color:'rebeccapurple'}}>Hero {props.name}</h1>
+    <h3 style={{color:'pink'}}>Hero of {props.Heroin}</h3>
+ </div>
+  )  
+   
 }
 
 export default App;
