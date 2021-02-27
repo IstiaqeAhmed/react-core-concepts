e1897f80613dcd7a48c61eeb1339b823fb13bb06
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-27 16:33:51 +0600

    32-8

--- src/App.js
+++ src/App.js
@@ -12,33 +12,47 @@ function App() {
     backgroundColor: 'blue'
   }
   const heroes= ['Anwar', 'Badshah', 'Elias','Salman']
+  const products= [
+    {name: 'Photoshop',  price:'$90.99'},
+     {name: 'Illustrator', price: '$60.99'},
+     {name: 'PDF Reader', price: '$5.99'}
+]
   return (
     <div className="App">
       <header className="App-header">
-        <p style={{color:'red'}}>Famous Hero and His Heroin</p>
-      <Person name= {heroes[0]}Heroin="MunMun"></Person>
-      <Person name="Alamgir" Heroin="Shabana"></Person>
-      <Person name="kabila" Heroin="Nachrin"></Person>
-      <Person name="Jasim" Heroin="Bobita"></Person>
-          
+        <p style={{color:'red'}}>Gadget X Official Store</p>
+        <Product product={products[0]}></Product>
+      <Product product={products[1]}></Product>
+      <Product product={products[2]}></Product>
+      {/* <Product name="Alamgir" Heroin="Shabana"></Product> */}
       </header>
     </div>
   );
 }
-function Person(props){
-  const personStyle={
-    border:'5px solid green',
-    borderRadius:'10px',
-    boxShadow: '0px 0px 30px 30px blue ',
-    width: '250px',
-    margin: '10px'
+function Product(props){
+  const productStyle={
+    border:'1px solid grey',
+    borderRadius:'5px',
+    backgroundColor:'lightgrey',
+    width: '200px',
+    height:'200px',
+    margin: '10px',
+    float: 'left'
   }
   // console.log(props)
+  //destructuring
+  const {name, price} =props.product;
+  //console.log(name, price)
   return (
-  <div style={personStyle}>
-    <h1  style={{color:'rebeccapurple'}}>Hero {props.name}</h1>
-    <h3 style={{color:'pink'}}>Hero of {props.Heroin}</h3>
- </div>
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy now</button>
+      {/* before destructuring */}
+      {/* <h3>{props.product.name}</h3>
+      <h5>{props.product.price}</h5>
+      <button>Buy now</button> */}
+    </div>
   )  
    
 }
